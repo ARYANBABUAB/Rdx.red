6b159e6b586a681fdef0a853a2d2c5800d5ad64d
ARYANBABUAB <157523788+ARYANBABUAB@users.noreply.github.com>
2026-01-22 16:56:27 +0500

    Update 0_RDX_OWNER.js

--- rdx/commands/0_RDX_OWNER.js
+++ rdx/commands/0_RDX_OWNER.js
@@ -30,24 +30,24 @@ module.exports = {
 ║   ✨ 𝐁𝐎𝐓 𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎 ✨   ║
 ╠═══════════════════════════╣
 ║                           ║
-║  👤 𝐍𝐚𝐦𝐞: SARDAR RDX    ║
+║  👤 𝐍𝐚𝐦𝐞: ARYAN BABU    ║
 ║                           ║
 ╠═══════════════════════════╣
 ║  📱 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐈𝐧𝐟𝐨:          ║
 ║                           ║
 ║  🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤:              ║
-║  facebook.com/SARDAR RDX ║
+║  https://www.facebook.com/babu.55414║
 ║                           ║
 ║  📲 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩:              ║
-║  wa.me/923301068874       ║
+║  wa.me/0308******* ║
 ║                           ║
 ╠═══════════════════════════╣
 ║  🤖 𝐁𝐨𝐭 𝐃𝐞𝐭𝐚𝐢𝐥𝐬:           ║
 ║                           ║
-║  📛 Name: ${config.BOTNAME || 'SARDAR RDX'}
-║  ⚡ Prefix: ${config.PREFIX || '.'}
-║  💻 Version: 0.5       ║
-║  🛠️ Framework: RDX-FCA    ║
+║  📛 Name: ${config.BOTNAME || 'SHONA BOT'}
+║  ⚡ Prefix: ${config.PREFIX || '*'}
+║  💻 Version: 0.8       ║
+║  🛠️ Framework: SINNER-FCA    ║
 ║                           ║
 ╠═══════════════════════════╣
 ║  💝 𝙏𝙝𝙖𝙣𝙠 𝙮𝙤𝙪 𝙛𝙤𝙧 𝙪𝙨𝙞𝙣𝙜!  ║
